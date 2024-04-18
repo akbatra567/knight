7619c8e52553fb89ee7a79f4660de25cd56cef7b
Akshit Batra <akbatra567@gmail.com>
2024-04-18 23:31:38 +0530

    feat: Setup flyway for db migrations

---
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    id int,
+    first_name varchar(240),
+    last_name varchar(240),
+    email_address varchar(240),
+    username varchar(240),
+    password varchar(240)
+);
